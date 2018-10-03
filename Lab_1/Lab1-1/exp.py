c9cbff764a782bf1dcce67487a5f1765404f7973
Yasheed1995 <qwerty8608301@gmail.com>
2018-10-03 13:25:51 +0800

    regular commit

--- Lab_1/Lab1-1/exp.py
+++ Lab_1/Lab1-1/exp.py
@@ -3,15 +3,47 @@ from IPython import embed
 
 context.arch = 'amd64'
 
-r = remote('127.0.0.1', 8888)
+r = remote('csie.ctf.tw', 10123)
+while(True):
 
-a = asm("""
-        push 0x68
-        mov rax, 0x732f2f2f6e69622f
-        push rax
-        """)
+    try:
+        a = r.recvuntil('left', drop=True)
+        b = r.recvuntil('@', timeout=5)
 
-e = ELF('/lib/x86-64-linux-gnu/libc.so.6')
-embed()
+        print (a)
 
+        print ("============================")
+        print (b)
+        print ("============================")
+
+        c = b.split()
+        c = c[:-1]
+        print (c)
+        c = [int(x) for x in c]
+        d = ''
+        for i in sorted(c):
+ 	       d += str(i)
+ 	       d += ' '
+
+        r.sendline(d)
+    except EOFError:
+        print ("HI")
+        break
+'''
+print (a)
+#print ("========", a, "==========")
+b = r.recv(1000)
+print
+print ("============================")
+print (b)
+c = r.recv(1000)
+print
+print (c)
+
+d = r.recv(1000)
+print
+print (d)
+
+print ("============================")
+'''
 r.interactive()
